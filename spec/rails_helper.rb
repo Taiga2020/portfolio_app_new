7139d4d1fa8891402b987bdf06b4c5ba3a1fe63d
Taiga2020 <63719647+Taiga2020@users.noreply.github.com>
2020-07-07 19:58:40 +0900

    Revert "User controller"

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,9 +31,6 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
-  # ApplicationHelperクラスのメソッド（full_title）を自由に使えるようにする
-  config.include ApplicationHelper
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
