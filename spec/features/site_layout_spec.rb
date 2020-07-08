82bad6470a83886d1c51a1d60e3fc851f91d45ca
Taiga2020 <tech2020future@gmail.com>
2020-07-08 11:07:21 +0900

    Create signup-page

--- spec/features/site_layout_spec.rb
+++ spec/features/site_layout_spec.rb
@@ -2,19 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "SiteLayouts", type: :feature do
   describe "home layout" do
-    it "contains root link" do
+    before do
       visit root_path
+    end
+    it "contains root link" do
       expect(page).to have_link nil, href: root_path, count: 2
     end
-
     it "contains about link" do
-      visit about_path
       expect(page).to have_link 'About', href: about_path
     end
-
     it "contains help link" do
-      visit help_path
       expect(page).to have_link 'Help', href: help_path
+    end
+    it "contains signup link" do
+      expect(page).to have_link 'アカウント登録', href: signup_path
+    end
+  end
+
+  describe "title layout" do
+    it "contains アカウント登録 in signup-page title" do
+      # visit signup_path
+      pending "不明点：expect(title)??"
     end
   end
 end
