45dfe5bedc79d8dc06be0040cec57607a41a48d1
Taiga2020 <tech2020future@gmail.com>
2020-09-12 16:10:25 +0900

    AWS capistrano test3

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,8 @@ append :linked_files, "config/master.key"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 # シンボリックリンクのディレクトリを生成
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "public/storage"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "public/storage"
+append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/packs", "node_modules"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
