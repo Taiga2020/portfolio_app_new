b51df818911e1a6be560460a77c3ae67824312af
Taiga2020 <tech2020future@gmail.com>
2020-09-17 19:54:30 +0900

    Recreate config/puma.rb, Add bind.

--- config/puma.rb
+++ config/puma.rb
@@ -10,12 +10,12 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-# port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 } #コメントアウト
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock" #追記
 
 # Specifies the `environment` that Puma will run in.
 #
-# environment ENV.fetch("RAILS_ENV") { "development" } #変更
-environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" } #変更
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
@@ -26,14 +26,14 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 2 } #変更
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-preload_app! #変更
+# preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
