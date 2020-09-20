d6713e8406ada75287942156a5e9ae2033d4177f
Taiga2020 <tech2020future@gmail.com>
2020-09-20 11:09:03 +0900

    Finish editing rspec files to support mysql

--- spec/requests/users_edits_spec.rb
+++ spec/requests/users_edits_spec.rb
@@ -63,10 +63,13 @@ RSpec.describe "UsersEdits", type: :request do
         log_in_as(user)
         expect(is_logged_in?).to be_truthy
         get edit_user_path(user)
-        expect(request.fullpath).to eq '/users/1/edit'
+        # expect(request.fullpath).to eq '/users/1/edit'
+        expect(response.body).to include 'プロフィール編集'
+        expect(response.body).to include 'Michael Example'
         patch_invalid_information
         expect(flash[:danger]).to be_truthy
-        expect(request.fullpath).to eq '/users/1'
+        # expect(request.fullpath).to eq '/users/1' #表示は編集画面のまま
+        expect(response.body).to include 'プロフィール編集'
       end
     end
 
@@ -75,20 +78,24 @@ RSpec.describe "UsersEdits", type: :request do
         log_in_as(user)
         expect(is_logged_in?).to be_truthy
         get edit_user_path(user)
-        expect(request.fullpath).to eq '/users/1/edit'
+        # expect(request.fullpath).to eq '/users/1/edit'
+        expect(response.body).to include 'プロフィール編集'
         patch_valid_information
         expect(flash[:success]).to be_truthy
         follow_redirect!
-        expect(request.fullpath).to eq '/users/1'
+        # expect(request.fullpath).to eq '/users/1' #表示はマイページ
+        expect(response.body).to include 'マイページ'
+        expect(response.body).to include 'Michael Example'
       end
 
       it "shows previous page after login as correct user" do
-        get edit_user_path(user)
+        get edit_user_path(user) #プロフィール編集画面
         follow_redirect!
         expect(request.fullpath).to eq '/login'
         log_in_as(user)
         # expect(request.fullpath).to eq '/users/1' ではなく…
-        expect(request.fullpath).to eq '/users/1/edit'
+        # expect(request.fullpath).to eq '/users/1/edit' に遷移する
+        expect(response.body).to include 'プロフィール編集'
       end
     end
   end
